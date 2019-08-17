8aa7ad6d002aa7ef26d04b3ed67c2794e7060e32
David Thatcher <david.thatcher@cgius.com>
2019-08-17 12:42:17 -0400

    new file:   DevOps/gitlab/destroy.sh 	new file:   DevOps/gitlab/docker-compose.yml 	new file:   DevOps/gitlab/pragmatic-aegis-249716-ff618a4caf3b.json 	new file:   DevOps/install.sh 	new file:   DevOps/jenkins/destroy.sh 	new file:   DevOps/jenkins/docker-compose.yml 	new file:   DevOps/nexus/docker-compose.yml 	new file:   DevOps/sonar/docker-compose.yml

---
+++ DevOps/install.sh
@@ -0,0 +1,9 @@
+
+cd gitlab
+docker-compose up -d
+cd /c/Users/david.thatcher/DevOps/jenkins
+docker-compose up -d
+cd /c/Users/david.thatcher/DevOps/sonar
+docker-compose up -d
+cd /c/Users/david.thatcher/DevOps/nexus
+docker-compose up -d
